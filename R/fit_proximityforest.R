15432dab14650ffb02b4f325caa3ee35f92dca4b
Matthew Simmons <matthewsimmons@192-168-1-101.tpgi.com.au>
2020-11-21 15:24:12 +1100

    Progress! I have a better idea now of how to create one node - now to figure out how to make multiple.

--- R/fit_proximityforest.R
+++ R/fit_proximityforest.R
@@ -1,15 +1,32 @@
-data <- FreezerRegularTrain_TRAIN
+data <- tsforest::FreezerRegularTrain_TRAIN
+#
+# Remaining questions:
+# - How do you store the splits (as a tree of indexes?)
+# - need to be able recursively calculate many different distance measures and exemplars
+# - how do you apply it recursively
+# - how do you split the data on a new data set (store the exemplars and distance measures at each split?)
+# - build a proximity forest (not just a tree) and the voting mechanism
+
 
 fit_proximityforest <- function(data, ...) {
   X_df <- data[,!colnames(data) == "target"]
 
-  X_df_1 <- X_df[data$target == 1,]
-  X_df_0 <- X_df[data$target == 2,]
-  dist_1 <- as.matrix(dist(X_df_1, method = "euclidean"))
-  dist_0 <- as.matrix(dist(X_df_0, method = "euclidean"))
-  exemplar_1_idx <- which.min(rowMeans(dist_1))
-  examplar_0_idx <- which.min(rowMeans(dist_0))
-  examplar_1_ts <- X_df_1[exemplar_1_idx,]
-  examplar_0_ts <- X_df_0[exemplar_1_idx,]
+  X_df_1_idx <- which(data$target == 1)
+  X_df_0_idx <- which(data$target == 2)
+  distance_measures <- c("euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski")
+  random_measure <- sample(distance_measures, 1)
+  random_ts_1_idx <- sample(X_df_1_idx, 1)
+  random_ts_0_idx <- sample(X_df_0_idx, 1)
+  distance_matrix <- as.matrix(dist(X_df, method = random_measure))
 
+  X_df_1_dist <- distance_matrix[,random_ts_1_idx]
+  X_df_0_dist <- distance_matrix[,random_ts_0_idx]
+  combined_distances <- t(rbind(X_df_1_dist, X_df_0_dist))
+  split_idx <- which(combined_distances[,1] > combined_distances[,2])
+
+  L <- data[split_idx,]
+  R <- data[-split_idx,]
+  parent_impurity <- DescTools::Gini(data$target)
+  weighted_child_impurity <- (DescTools::Gini(L$target) * nrow(L) +
+    DescTools::Gini(R$target) * nrow(R))/nrow(data)
 }
